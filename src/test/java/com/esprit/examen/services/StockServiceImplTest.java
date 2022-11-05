c58ae33f858f1798270a5191bfed285c692fb6af
wassimbenbrahim <benbrahimwassim79@gmail.com>
2022-11-05 21:54:51 +0100

    pom99

--- src/test/java/com/esprit/examen/services/StockServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/StockServiceImplTest.java
@@ -3,6 +3,8 @@ package com.esprit.examen.services;
 import static org.junit.Assert.*;
 import java.util.List;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Order;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,20 +17,29 @@ public class StockServiceImplTest {
 	@Autowired
 	IStockService stockService;
 	
+
 	@Test
+	@Order(4)
+	public void testRetrieveAllStocks() {
+		List<Stock> listStocks = stockService.retrieveAllStocks();
+		Assertions.assertEquals(0, listStocks.size());
+	}
+	@Test
+	@Order(1)
 	public void testAddStock() {
 		List<Stock> stocks = stockService.retrieveAllStocks();
 		int expected=stocks.size();
 		Stock s = new Stock("stock test",10,100);
 		Stock savedStock= stockService.addStock(s);
-		
+
 		assertEquals(expected+1, stockService.retrieveAllStocks().size());
 		assertNotNull(savedStock.getLibelleStock());
 		stockService.deleteStock(savedStock.getIdStock());
-		
-	} 
-	
+
+	}
+
 	@Test
+	@Order(3)
 	public void testAddStockOptimized() {
 
 		Stock s = new Stock("stock test",10,100);
@@ -37,15 +48,15 @@ public class StockServiceImplTest {
 		assertSame(10, savedStock.getQte());
 		assertTrue(savedStock.getQteMin()>0);
 		stockService.deleteStock(savedStock.getIdStock());
-		
-	} 
-	
+
+	}
+
 	@Test
+	@Order(2)
 	public void testDeleteStock() {
 		Stock s = new Stock("stock test",30,60);
 		Stock savedStock= stockService.addStock(s);
 		stockService.deleteStock(savedStock.getIdStock());
 		assertNull(stockService.retrieveStock(savedStock.getIdStock()));
 	}
-
 }
